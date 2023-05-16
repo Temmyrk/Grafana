5f26a4f00b24c854db5559a75fa5c71e19fc870f
Julien Duchesne <julien.duchesne@grafana.com>
2023-05-16 13:01:57 -0400

    working

--- pkg/jsonnet/implementation/rustimpl/impl.go
+++ pkg/jsonnet/implementation/rustimpl/impl.go
@@ -1,59 +1,39 @@
 package rustimpl
 
-// #cgo CFLAGS: -I /Users/julienduchesne/Repos/jrsonnet
-// #cgo LDFLAGS: -L /Users/julienduchesne/Repos/jrsonnet/target/x86_64-unknown-linux-gnu/release/ -ljsonnet
-// #include "bindings/c/libjsonnet.h"
-import "C"
-
 import (
-	"errors"
-
 	"github.com/grafana/tanka/pkg/jsonnet/implementation/types"
+	"github.com/grafana/tanka/pkg/jsonnet/native"
 )
 
-type JsonnetRustVM struct {
-	vm *C.struct_JsonnetVm
-}
-
-func (vm *JsonnetRustVM) EvaluateAnonymousSnippet(filename, snippet string) (string, error) {
-	errorInt := C.int(0)
-	result := C.jsonnet_evaluate_snippet(vm.vm, C.CString(filename), C.CString(snippet), &errorInt)
-	if errorInt != 0 {
-		return "", errors.New(C.GoString(result))
-	}
-	return C.GoString(result), nil
-}
-
-func (vm *JsonnetRustVM) EvaluateFile(filename string) (string, error) {
-	errorInt := C.int(0)
-	result := C.jsonnet_evaluate_file(vm.vm, C.CString(filename), &errorInt)
-	if errorInt != 0 {
-		return "", errors.New(C.GoString(result))
-	}
-	return C.GoString(result), nil
+// EvaluateAnonymousSnippet does the same as `EvaluateSnippet`.
+// This is an alias to respect the interface and keep the CGO code intact from upstream.
+func (vm *VM) EvaluateAnonymousSnippet(filename, snippet string) (string, error) {
+	return vm.EvaluateSnippet(filename, snippet)
 }
 
 type JsonnetRustImplementation struct{}
 
 func (i *JsonnetRustImplementation) MakeVM(importPaths []string, extCode map[string]string, tlaCode map[string]string, maxStack int) types.JsonnetVM {
-	return &JsonnetRustVM{
-		vm: makeVM(importPaths, extCode, tlaCode, maxStack),
-	}
-}
-
-func makeVM(importPaths []string, extCode map[string]string, tlaCode map[string]string, maxStack int) *C.struct_JsonnetVm {
-	vm := C.jsonnet_make()
+	vm := Make()
 	for _, path := range importPaths {
-		C.jsonnet_jpath_add(vm, C.CString(path))
+		vm.JpathAdd(path)
 	}
 	for key, value := range extCode {
-		C.jsonnet_ext_code(vm, C.CString(key), C.CString(value))
+		vm.ExtCode(key, value)
 	}
 	for key, value := range tlaCode {
-		C.jsonnet_tla_code(vm, C.CString(key), C.CString(value))
+		vm.TlaCode(key, value)
 	}
 	if maxStack > 0 {
-		C.jsonnet_max_stack(vm, C.uint(maxStack))
+		vm.MaxStack(uint(maxStack))
 	}
+	for _, nf := range native.Funcs() {
+		params := []string{}
+		for _, p := range nf.Params {
+			params = append(params, string(p))
+		}
+		vm.NativeCallback(nf.Name, params, nf.Func)
+	}
+
 	return vm
 }
